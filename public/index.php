b479c00c8440b441b30cb59da96a02dfe20abe97
jolujuan <thesameexample@outlook.es>
2025-12-16 22:16:56 +0100

    Fix dotenv in railway

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use App\Kernel;
 
 //Dotenv carga las variables de los ficheros .env
 //Lo desactivamos en produccion, porque Railway define las variables globales
-if (($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) === 'prod') {
+if (!is_file(dirname(__DIR__).'/.env')) {
     $_SERVER['APP_RUNTIME_OPTIONS']['disable_dotenv'] = true;
 }
 
