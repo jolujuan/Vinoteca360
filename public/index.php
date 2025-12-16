4349ab9e95e864d423f51f6f0c007aaa2df14a80
jolujuan <thesameexample@outlook.es>
2025-12-16 20:47:25 +0100

    Fix .env in railway

--- public/index.php
+++ public/index.php
@@ -2,14 +2,14 @@
 
 use App\Kernel;
 
-if (($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) === 'prod') {
+if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV']) || (($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? null) === 'prod')) {
     $_SERVER['APP_RUNTIME_OPTIONS'] = [
         'disable_dotenv' => true,
     ];
 }
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
